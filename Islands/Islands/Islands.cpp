164b567b2bccfc336a858c5fc9c87ff233c0aa71
RomashkoVictor <phantom.rvr@gmail.com>
2015-04-20 21:25:59 +0300

    Islands martix izomorf

--- Islands/Islands/Islands.cpp
+++ Islands/Islands/Islands.cpp
@@ -4,16 +4,12 @@
 #include<ctime>
 using namespace std;
 
-struct Bridge
-{
-	int city1;
-	int city2;
-	int cost;
-};
-
 int numIslands,numBridges;
 int cities[3];
-Bridge *bridges;
+int **bridges;
+int way[3];
+int *cost;
+bool *mark;
 
 void input(char fileName[])
 {
@@ -26,22 +22,128 @@ void input(char fileName[])
 		exit(1);
 	}
 	fscanf(file, "%d%d", &numIslands, &numBridges);
-	bridges = new Bridge[numBridges];
+	mark = new bool[numIslands];
+	cost = new int[numIslands];
+	bridges = new int*[numIslands];
+	for(int i = 0; i < numIslands; i++)
+		bridges[i] = new int[numIslands];
+	for(int i = 0; i < numIslands; i++)
+	{	
+		for( int j = 0; j < numIslands; j++)
+			bridges[i][j] = -1;
+		bridges[i][i] = 0;
+		cost[i] = INT_MAX;
+		mark[i] = true;
+	}
+	int city1,city2,tempCost;
 	for(int i = 0; i < numBridges; i++)
-		fscanf(file, "%d%d%d", &bridges[i].city1, &bridges[i].city2, &bridges[i].cost);
-	fscanf(file, "%d%d%d", &cities[0], &cities[1], cities[2]);
+	{
+		fscanf(file, "%d%d%d", &city1, &city2, &tempCost);
+		bridges[city1][city2] = tempCost;
+		bridges[city2][city1] = tempCost;
+	}
+	way[0] = way[1] = way[2] = -10;
+	fscanf(file, "%d%d%d", &cities[0], &cities[1], &cities[2]);
 	fclose(file);
 }
 
-void dijkstra(int start)
+int changeCurrentCity()
 {
+	int min=-1;
+	for(int i=0;i<numIslands;i++)
+	{
+		if((mark[i])&&(cost[i]!=INT_MAX))
+		{
+			min=i;
+			for (; i < numIslands; i++)
+				if( (mark[i]) && (cost[i] < cost[min]) )
+					min=i;
+		}
+	}
+	return min;
+}
 
+void dijkstra(int start, int end1, int end2)
+{
+	int currentCity = start;
+	cost[currentCity] = 0;
+	while( ( (way[0] == -1) || (way[1] == -1) ) && (currentCity > -1) )
+	{
+		for (int i = 0; i < numIslands; i++)
+		{
+			if (mark[i])
+			{
+				if ( (bridges[currentCity][i] > 0) && ( (cost[currentCity] + bridges[currentCity][i]) < cost[i]) )
+				{
+					cost[i] = cost[currentCity] + bridges[currentCity][i];
+					//way[i] = currentCity; 
+				}
+			}
+		}
+		currentCity = changeCurrentCity();
+		if(currentCity!=-1)
+			mark[currentCity] = false;
+		if(currentCity == end1)
+			way[0] = cost[currentCity];
+		if(currentCity == end2)
+			way[1] = cost[currentCity];
+	}
+	for(int i = 0; i < numIslands; i++)
+	{	
+		cost[i] = INT_MAX;
+		mark[i] = true;
+	}
+}
+
+void dijkstra(int start, int end1)
+{
+	int currentCity = start;
+	cost[currentCity] = 0;
+	while( (way[2] == -1) && (currentCity > -1) )
+	{
+		for (int i = 0; i < numIslands; i++)
+		{
+			if (mark[i])
+			{
+				if ( (bridges[currentCity][i] > 0) && ( (cost[currentCity] + bridges[currentCity][i]) < cost[i]) )
+				{
+					cost[i] = cost[currentCity] + bridges[currentCity][i];
+					//way[i] = currentCity; 
+				}
+			}
+		}
+		currentCity = changeCurrentCity();
+		if(currentCity!=-1)
+			mark[currentCity] = false;
+		if(currentCity == end1)
+			way[2] = cost[currentCity];
+	}
+}
+
+int min()
+{
+	if( way[0] < way[1])
+	{
+		return way[0] + (way[1]<way[2]?way[1]:way[2]);
+	}
+	else
+	{
+		return way[1] + (way[0]<way[2]?way[0]:way[2]);
+	}
 }
 
 int main()
 {
 	input("input.txt");
-
+	dijkstra(cities[0], cities[1], cities[2]);
+	dijkstra(cities[1], cities[2]);
+	ofstream out("output.txt");
+	out<<min()<<endl;
+	out.close();
+	delete cost;
+	delete mark;
+	for(int i = 0; i < numIslands; i++)
+		delete bridges[i];
 	delete bridges;
 	system("pause");
 	return 0;
